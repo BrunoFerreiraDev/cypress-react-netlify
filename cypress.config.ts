ca237194ad9e30bd0a362599e668763b546b3466
Bruno Ferreira Santos <brunoferreirasmv@gmail.com>
2023-04-20 12:35:05 -0400

    test

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
-  projectId: 'j6uxuv',
+  projectId: "j6uxuv",
+  env: {
+    CYPRESS_RECORD_KEY: "CYPRESS_RECORD_KEY_TEST",
+  },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
