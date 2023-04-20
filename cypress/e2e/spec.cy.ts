68e5642e6dfd43177f8c0cc55cd41208a0f4986d
Bruno Ferreira Santos <brunoferreirasmv@gmail.com>
2023-04-20 12:08:12 -0400

    creted app test

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -2,6 +2,6 @@ describe("template spec", () => {
   it("passes", () => {
     cy.visit("https://scintillating-pixie-f7558d.netlify.app");
 
-    cy.contains("Edit src/App.tsx and save to reload.");
+    cy.contains("Edit src/App.tsx aaand save to reload.");
   });
 });
