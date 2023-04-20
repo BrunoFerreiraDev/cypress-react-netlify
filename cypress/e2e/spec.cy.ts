613bac8e33a870f6cde436e0bd3b7dbae5da7417
Bruno Ferreira Santos <brunoferreirasmv@gmail.com>
2023-04-20 11:17:35 -0400

    creted app test

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -4,6 +4,6 @@ describe("template spec", () => {
       "https://644155b63b008e000805553e--brilliant-praline-704934.netlify.app"
     );
 
-    cy.contains("Edit src/App.tsx and save to reload.");
+    cy.contains("Edit src/App.tsx ssss and save to reload.");
   });
 });
